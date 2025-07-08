e1fd4b0530f6dab0f7a0c2eac2892f6505dc855c
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2025-07-08 21:09:11 +0530

    initial backend setup done

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       <div className="flex justify-between items-center max-w-6xl lg:max-w-7xl mx-auto p-4">
         <Link to={"/"}>
           <h1 className="font-bold text-xl sm:text-2xl flex flex-wrap">
-            <span className="text-slate-500">Morning</span>
-            <span className="text-slate-900">Dispatch</span>
+            <span className="text-slate-500">Tech</span>
+            <span className="text-slate-900">Express</span>
           </h1>
         </Link>
 
